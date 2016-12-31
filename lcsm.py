2590c8a8cd2284cbfb722988f0ed9d69c82aee89
vaastav <vaastav.anand05@gmail.com>
2016-12-31 05:15:54 -0800

    Solve Finding a Shared Motif

--- lcsm.py
+++ lcsm.py
@@ -1,22 +1,33 @@
 import sys
-from pprint import pprint
 
-strings = {}
+strings = []
+
+def lcs(strings):
+	substr = ''
+	if len(strings) > 1 and len(strings[0]) > 0:
+		for i in range(len(strings[0])):
+			for j in range(len(strings[0]) - i + 1):
+					if j > len(substr) and all(strings[0][i:i+j] in x for x in strings):
+						substr = strings[0][i:i+j]
+
+	return substr
 
 def read_file(filename):
 	inf = open(filename,'rU')
 	name = ''
 	protein = ''
+	global strings
 	for line in inf:
 		line = line.strip()
 		if line[0] == '>':
 			if name != '':
-				strings[name] = protein
+				strings += [protein]
 			name = line[1:]
 			protein = ''
 		else:
 			protein += line
-	strings[name] = protein
+	strings += [protein]
+	print(lcs(strings))
 
 def main():
 	read_file(sys.argv[1])
