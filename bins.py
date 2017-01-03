bff9d9e438582312677359ebce78fb1882a808fe
vaastav <vaastav.anand05@gmail.com>
2017-01-03 10:24:09 -0800

    Finish binary search

--- bins.py
+++ bins.py
@@ -1,9 +1,38 @@
 import sys
 
+def binary(A,key,first,last):
+	if first > last:
+		return -1
+	mid = int((first + last)/2)
+	if A[mid] == key:
+		return mid
+	else:
+		if key > A[mid]:
+			return binary(A,key,mid+1,last)
+		elif key < A[mid]:
+			return binary(A,key,first,mid-1)
+
+
 def readFile(filename):
-	inf = open(fielname,'rU')
+	inf = open(filename,'rU')
+	A = []
+	K = []
+	indices = []
 	n = int(inf.readline())
 	m = int(inf.readline())
+	line = inf.readline().split(' ')
+	for num in line:
+		A += [int(num)]
+	line = inf.readline().split(' ')
+	for num in line:
+		K += [int(num)]
+	for key in K:
+		result = binary(A,key,0,n-1)
+		if result != -1:
+			indices += [result + 1]
+		else:
+			indices += [result]
+	print(' '.join(map(str,indices)))
 
 def main():
 	readFile(sys.argv[1])
