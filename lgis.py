4b8d7a19edbe7094d18d4f0877082b8181d7c7dd
Vaastav Anand <vaastav.anand05@gmail.com>
2022-01-31 16:35:32 +0100

    Fix sequence finding bugs in lgis

--- lgis.py
+++ lgis.py
@@ -1,27 +1,35 @@
 import sys
 
-def find_sequence(arr,sequence):
+def find_dec_sequence(arr, sequence):
 	seq = []
-	print(arr)
-	for i in range(len(arr)):
-		if i == len(arr)-1:
-			if arr[i] != arr[i-1]:
-				seq += [sequence[i]]
-		else:
-			if arr[i+1] == arr[i] + 1:
-				seq += [sequence[i]]
-
-	return seq
+	max_value = max(arr)
+	for i in range(len(arr)-1, -1, -1):
+		if max_value == arr[i]:
+			seq += [sequence[i]]
+			max_value -= 1
+	return seq[::-1]
 
 def lis(n,sequence):
 	lis = [1]*n
+	p = [-1] * n
 
 	for i in range(1, n):
 		for j in range(0, i):
 			if sequence[i] > sequence[j] and lis[i] < lis[j] + 1:
 				lis[i] = lis[j] + 1
+				p[i] = j
 
-	return find_sequence(lis,sequence)
+	ans = lis[0]
+	pos = 0
+	for i in range (1, n):
+		if lis[i] > ans:
+			ans = lis[i]
+			pos = i
+	subseq = []
+	while pos != -1:
+		subseq += [sequence[pos]]
+		pos = p[pos]
+	return subseq[::-1]
 
 def lds(n,sequence):
 	lds = [1]*n
@@ -31,14 +39,15 @@ def lds(n,sequence):
 			if sequence [i] < sequence[j] and lds[i] < lds[j] + 1:
 				lds[i] = lds[j] + 1
 
-	return find_sequence(lds,sequence)
+	return find_dec_sequence(lds,sequence)
 
 def readFile(filename):
-	inf = open(filename,'rU')
+	inf = open(filename,'r')
 	n = int(inf.readline().strip())
 	sequence = inf.readline().strip().split(' ')
-	print(lis(n,sequence))
-	print(lds(n,sequence))
+	sequence = [int(s) for s in sequence]
+	print(' '.join([str(s) for s in lis(n,sequence)]))
+	print(' '.join([str(s) for s in lds(n,sequence)]))
 
 def main():
 	readFile(sys.argv[1])
